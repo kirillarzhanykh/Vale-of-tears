1e828de01ef0886551c9d6f6c1955bd333b2b0ae
Аржаных Кирилл <kirill.arzhanykh@math.msu.ru>
2023-10-03 20:38:40 +0300

    Теперь дело за математикой

---
+++ sem_5/1/solver_H.hpp
@@ -0,0 +1,4 @@
+
+int solver(int n, double* A, double* b, double* x){
+    return 0;
+}
--- sem_5/1/test.cpp
+++ sem_5/1/test.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <cstring>
-#include <cmath>
+#include "matrix_creator.hpp"
+#include "solver_H.hpp"
 #include <random>
 #include <time.h>
 
-int matrix_reader(int n, double* matrix, char* filename);
+int matrix_writer(int a, int b, int m, double* matrix);
+
+int right_side_creator(int n, double* matrix, double* right_side);
 
 int main(int argc, char* argv[]){
    
@@ -12,54 +15,52 @@ int main(int argc, char* argv[]){
         std::cout << "дурак" << std::endl;
     } 
     int n = argv[1][0] - '0';
-    //int m = argv[2][0] - '0';
+    int m = argv[2][0] - '0';
     int k = argv[3][0] - '0';
     char filename[20];
     if(argc == 5){
         strcpy(filename, argv[4]);
     }
+
     double* M;
     M = new double[n*n];
+    matrix_creator(n, k, filename, M);
 
-    if(k == 0){
-        if(matrix_reader(n, M, filename) != 0){
-            std::cout << "Ошибка чтения матрицы из файла!" << std::endl;
-        }
-    }
-    if(k == 1){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                M[i * n + j] = n - std::max(i, j);
-            }
-        }
-    }
-    if(k == 2){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                M[i * n + j] = std::max(i + 1, j + 1);
-            }
-        }
-    }
-    if(k == 3){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                M[i * n + j] = abs(i - j);
-            }
-        }
-    }
-    if(k == 4){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                M[i * n + j] = 1 / ( i + j + 3);
-            }
-        }
-    }
+    double* b;
+    b = new double[n];
+    right_side_creator(n, M, b);
 
+    double* Res;
+    Res = new double[n];
 
+    solver(n, M, b, Res);
 
+    matrix_writer(n, n, m, M);
+    matrix_writer(n, 1, m, b);
+    delete[] M;
+    delete[] b;
+    delete[] Res;
     return 0;
 }
 
-int matrix_reader(int n, double* matrix, char* filename){
+
+int matrix_writer(int a, int b, int m, double* matrix){
+    for(int i = 0; i < std::min(m, a); i++){
+        std::cout << "\n";
+        for(int j = 0; j < std::min(m, b); j++){
+            std::cout << matrix[i * b + j] << "   ";
+        }
+        std::cout << "\n";   
+    } 
+    return 0;
+}
+
+int right_side_creator(int n, double* matrix, double* right_side){
+    for(int i = 0; i < n; i++){
+        right_side[i] = 0;
+        for(int j = 0; j < n; j += 2){
+            right_side[i] += matrix[i * n + j + 1];
+        }
+    }
     return 0;
 }
