ddb30384aee356f483e5a1a8fe1fe9a13a68160a
Аржаных Кирилл <kirill.arzhanykh@math.msu.ru>
2023-10-04 17:35:53 +0300

    Ну теперь точно одна математика осталась

--- sem_5/1/solver_H.hpp
+++ sem_5/1/solver_H.hpp
@@ -1,4 +1,12 @@
 
 int solver(int n, double* A, double* b, double* x){
+    
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            for(int k = 0; k < n; k++){
+            
+            }
+        }
+    }
     return 0;
 }
--- sem_5/1/test.cpp
+++ sem_5/1/test.cpp
@@ -9,14 +9,18 @@ int matrix_writer(int a, int b, int m, double* matrix);
 
 int right_side_creator(int n, double* matrix, double* right_side);
 
+double norm(int n, double* vector);
+
+double relative_norm(int n, double* first_vector, double* second_vector);
+
 int main(int argc, char* argv[]){
    
     if (argc < 4){
         std::cout << "дурак" << std::endl;
     } 
-    int n = argv[1][0] - '0';
-    int m = argv[2][0] - '0';
-    int k = argv[3][0] - '0';
+    int n = atoi(argv[1]);
+    int m = atoi(argv[2]);
+    int k = atoi(argv[3]);
     char filename[20];
     if(argc == 5){
         strcpy(filename, argv[4]);
@@ -30,25 +34,43 @@ int main(int argc, char* argv[]){
     b = new double[n];
     right_side_creator(n, M, b);
 
+    double* Res_Real;
+    Res_Real = new double[n];
+    for(int i = 0; i < n; i++){
+        Res_Real[i] = 0;
+        Res_Real[i] += ((i + 1) % 2);
+    }
+
     double* Res;
     Res = new double[n];
 
+    clock_t start = clock();
     solver(n, M, b, Res);
+    clock_t end = clock();
+    double seconds = (end - start) / CLOCKS_PER_SEC;
+
 
     matrix_writer(n, n, m, M);
     matrix_writer(n, 1, m, b);
+    std::cout << "\n";
+    std::cout << "Норма невязки (относительная): " << relative_norm(n, Res, Res_Real) << std::endl;
+    std::cout << "\n";
+    std::cout << "Время решения системы: " << seconds << " секунд" << std::endl;
     delete[] M;
     delete[] b;
+    delete[] Res_Real;
     delete[] Res;
     return 0;
 }
 
 
 int matrix_writer(int a, int b, int m, double* matrix){
+    int z;
     for(int i = 0; i < std::min(m, a); i++){
         std::cout << "\n";
         for(int j = 0; j < std::min(m, b); j++){
-            std::cout << matrix[i * b + j] << "   ";
+            z = matrix[i * b + j] * 1000;
+            std::cout << z << "%10.3e   ";
         }
         std::cout << "\n";   
     } 
@@ -59,8 +81,30 @@ int right_side_creator(int n, double* matrix, double* right_side){
     for(int i = 0; i < n; i++){
         right_side[i] = 0;
         for(int j = 0; j < n; j += 2){
-            right_side[i] += matrix[i * n + j + 1];
+            right_side[i] += matrix[i * n + j];
         }
     }
     return 0;
+}
+
+double norm(int n, double* vector){
+    double Sq = 0;
+    for(int i = 0; i < n; i++){
+        Sq += pow(vector[i], 2);
+    }
+    return sqrt(Sq);
+}
+
+double relative_norm(int n, double* first_vector, double* second_vector){
+    double* difference;
+    double Res;
+    difference = new double[n];
+
+    for(int i = 0; i < n; i++){
+        difference[i] = first_vector[i] - second_vector[i];
+    }
+
+    Res = norm(n, difference) / norm(n, second_vector);
+    delete[] difference;
+    return Res;
 }
