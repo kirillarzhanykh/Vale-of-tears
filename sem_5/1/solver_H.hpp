fcd6ee3239a8837732b09eb1b6ba3282466a3dce
Аржаных Кирилл <kirill.arzhanykh@math.msu.ru>
2023-10-06 18:13:26 +0300

    Случай малых значений нужно еще раскидать

--- sem_5/1/solver_H.hpp
+++ sem_5/1/solver_H.hpp
@@ -1,12 +1,44 @@
 
 int solver(int n, double* A, double* b, double* x){
-    
+    double buf, buf2;
+    int num = 0;
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            for(int k = 0; k < n; k++){
-            
+        buf = 0;
+
+        //Выбираем строку с максимальным первым элементом
+        for(int j = i; j < n ; j++){
+            if(abs(A[j * n + i]) > buf) buf = abs(A[j * n + i]);
+            num = j; 
+        }
+
+        //Переносим эту строку наверх
+        for(int j = i; j < n ; j++){
+            buf = A[num * n + j];
+            A[num * n + j] = A[i * n + j];
+            A[i * n + j] = buf;
+        }
+        buf = b[num];
+        b[num] = b[i];
+        b[i] = buf;
+
+        //Вычитаем строку из нижеследующих
+        buf = A[i * n + i];
+        for(int j = i + 1; j < n; j++){
+            buf2 = A[j * n + i] / buf;
+            for(int k = i; k < n; k++){
+                A[j* n + k] -= A[i * n + k] * buf2;
             }
+            b[j] -= b[i] * buf2;
         }
     }
+
+    for(int i = n - 1; i >= 0; i--){
+        x[i] = b[i];
+        for(int j = n - 1; j > i; j--){
+            x[i] -= x[j] * A[i * n + j];
+        }
+        x[i] = x[i] / A[i * n + i];
+    }
+
     return 0;
 }
--- sem_5/1/test.cpp
+++ sem_5/1/test.cpp
@@ -13,6 +13,8 @@ double norm(int n, double* vector);
 
 double relative_norm(int n, double* first_vector, double* second_vector);
 
+void copy_filler(int n, double* from, double* to);
+
 int main(int argc, char* argv[]){
    
     if (argc < 4){
@@ -29,10 +31,16 @@ int main(int argc, char* argv[]){
     double* M;
     M = new double[n*n];
     matrix_creator(n, k, filename, M);
+    double* M_copy;
+    M_copy = new double[n*n];
+    copy_filler(n*n, M, M_copy);
 
     double* b;
     b = new double[n];
     right_side_creator(n, M, b);
+    double* b_copy;
+    b_copy = new double[n];
+    copy_filler(n, b, b_copy);
 
     double* Res_Real;
     Res_Real = new double[n];
@@ -45,19 +53,21 @@ int main(int argc, char* argv[]){
     Res = new double[n];
 
     clock_t start = clock();
-    solver(n, M, b, Res);
+    solver(n, M_copy, b_copy, Res);
     clock_t end = clock();
-    double seconds = (end - start) / CLOCKS_PER_SEC;
+    double seconds =  static_cast<double>(end - start) / CLOCKS_PER_SEC;
 
 
-    matrix_writer(n, n, m, M);
-    matrix_writer(n, 1, m, b);
+    //matrix_writer(n, n, m, M);
+    matrix_writer(n, 1, m, Res);
     std::cout << "\n";
     std::cout << "Норма невязки (относительная): " << relative_norm(n, Res, Res_Real) << std::endl;
     std::cout << "\n";
-    std::cout << "Время решения системы: " << seconds << " секунд" << std::endl;
+    std::cout << "Время решения системы: " << seconds << " сек" << std::endl;
     delete[] M;
+    delete[] M_copy;
     delete[] b;
+    delete[] b_copy;
     delete[] Res_Real;
     delete[] Res;
     return 0;
@@ -107,4 +117,10 @@ double relative_norm(int n, double* first_vector, double* second_vector){
     Res = norm(n, difference) / norm(n, second_vector);
     delete[] difference;
     return Res;
+}
+
+void copy_filler(int n, double* from, double* to){
+    for(int i = 0; i < n; i++){
+        to[i] = from[i];
+    }    
 }
