0319512759377ee408f83ae61994e8f3185e0208
Аржаных Кирилл <kirill.arzhanykh@math.msu.ru>
2023-10-21 12:50:14 +0300

    Метод тыка

--- sem_5/2/eigenvalues_QR.cpp
+++ sem_5/2/eigenvalues_QR.cpp
@@ -1,3 +1,6 @@
-int eigenvalues(int n, double* A, double* Res, double eps){
+int eigenvalues(int n, double* A, double* EigenValues, double eps){
+    for(int i = 0; i < n; i++){
+        EigenValues[i] = A[i * n + i];
+    }
     return 0;
 }
